bcb037b044aab99210ce1694566512a267ad42be
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-09-21 08:44:15 +0200

    add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import Providers from '@/components/providers'
 import { auth } from '@/lib/auth'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'ExpenseCal',
   description: 'Track your monthly and yearly expenses and view them in a calendar',
 }
